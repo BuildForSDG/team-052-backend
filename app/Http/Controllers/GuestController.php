afca38d91e06b8e839e94bd587b00ea857b4428c
kodnificent <mbamaravictor@gmail.com>
2020-06-11 18:34:56 +0100

    merge issue 1 with issue 2

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -19,31 +19,19 @@ class GuestController extends Controller
      */
     public function reports(Request $request)
     {
-        //@issue 2
-        // As a user, I want to be able to view recently reported incidents
-        $reports = Report::where('title', 'location', 'visual_image', 'time_of_report')
-        ->orderBy('time_of_report', 'desc')
-        ->simplePaginate();
-
-        // this method should get the recently reported incidents
-        // just like (new ReportController)->list() will do but the data returned
-        // should be limited to title, location, visual_image and time_report
-        // tip: use select() eloquent query method to select only the required columns
         if(!empty($request->status)){
-            $reports = Report::where('status', $request->status)->orderBy('id', 'desc')->simplePaginate();
+            $reports = Report::where('status', $request->status)->orderBy('id', 'desc');
         }elseif(!empty($request->time)){
-            $reports = Report::where('time_of_report', $request->time)->orderBy('id', 'desc')->simplePaginate();
+            $reports = Report::where('time_of_report', $request->time)->orderBy('id', 'desc');
         }elseif(!empty($request->location)){
-            $reports = Report::where('location', $request->location)->orderBy('id', 'desc')->simplePaginate();
+            $reports = Report::where('location', $request->location)->orderBy('id', 'desc');
         } else {
-            $reports = Report::orderBy('id', 'desc')->simplePaginate();
+            $reports = Report::orderBy('id', 'desc');
         }
+
+        $reports = $reports->select('title', 'location', 'visual_image', 'time_of_report')->simplePaginate();
+
         return $this->listResponse($this->extractItemsFrom($reports), $this->extractMetaFrom($reports));
-
-
-        // return a list response just like in the report controller
-        //return $this->listResponse($this->extractItemsFrom($reports), $this->extractMetaFrom($reports));
-
     }
 
     /**
@@ -62,13 +50,6 @@ class GuestController extends Controller
         $onsite_cases = Report::where('status', 'onsite')->count();
         $acknowledged_cases = Report::where('status', 'acknowledged')->count();
 
-        return response()->json([
-            'metrics' => view ('metrics')
-            ->with('reported_cases', $reported_cases)
-            ->with('pending_cases', $pending_cases)
-            ->with('enroute_cases', $enroute_cases)
-            ->with('onsite_cases', $onsite_cases)
-            ->with('acknowledged_cases', $acknowledged_cases)
-        ], 200);
+        return response()->json(compact('reported_cases', 'pending_cases', 'enroute_cases', 'onsite_cases', 'acknowledged_cases'), 200);
     }
 }
